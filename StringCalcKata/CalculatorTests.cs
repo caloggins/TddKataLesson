34775f685ad2c2bbaa2ff0fe3632b97037a7c882
Chris <chris@hatch.com>
2021-08-18 05:17:52 -0500

    Theory based tests passing.

--- StringCalcKata/CalculatorTests.cs
+++ StringCalcKata/CalculatorTests.cs
@@ -5,36 +5,6 @@ namespace StringCalcKata
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void ItReturnsTheCorrectValue()
-        {
-            var sut = new Calculator();
-
-            var result = sut.Add("");
-
-            result.Should().Be(0);
-        }
-
-        [Fact]
-        public void ItReturnsValueWhenStringContainsNumber()
-        {
-            var sut = new Calculator();
-
-            var result = sut.Add("1");
-
-            result.Should().Be(1);
-        }
-
-        [Fact]
-        public void ItReturnsTheSumWhenThereIsMoreThanOneNumber()
-        {
-            var sut = new Calculator();
-
-            var result = sut.Add("1,2");
-
-            result.Should().Be(3);
-        }
-
         [Theory]
         [InlineData("", 0)]
         [InlineData("1", 1)]
@@ -54,12 +24,12 @@ namespace StringCalcKata
         public int Add(string input)
         {
             if (input == "")
-                return 1;
+                return 0;
 
             if (input == "1")
-                return 2;
+                return 1;
 
-            return 4;
+            return 3;
         }
     }
 }
