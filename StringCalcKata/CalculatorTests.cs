d216c5a5c69fe9845c3351897e50bca82a304acc
Chris <chris@hatch.com>
2021-08-18 06:20:04 -0500

    Failed... Ignores numbers greater than 1000.

--- StringCalcKata/CalculatorTests.cs
+++ StringCalcKata/CalculatorTests.cs
@@ -18,6 +18,7 @@ namespace StringCalcKata
         [InlineData("1", 1)]
         [InlineData("1,2", 3)]
         [InlineData("1,2,3", 6)]
+        [InlineData("1,1001,3", 4)]
         [InlineData("1\n2,3", 6)]
         [InlineData("//;\n1;2;3", 6)]
         public void ItReturnsTheCorrectResult(string input, int expected)
