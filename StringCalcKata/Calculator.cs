a585a001919992e9fd233b32e8957d3d44efb8b4
Chris <chris@hatch.com>
2021-08-18 06:18:41 -0500

    Refactor... Removed the error handling.

--- StringCalcKata/Calculator.cs
+++ StringCalcKata/Calculator.cs
@@ -1,33 +1,22 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace StringCalcKata
 {
     public class Calculator
     {
         private readonly SumHandler sumHandler = new SumHandler();
+        private readonly NegativeNumberHandler negativeNumberHandler = new NegativeNumberHandler();
         
         public int Add(string input)
         {
             if (InputIsEmpty(input))
                 return 0;
 
-            CheckForNegatives(input);
+            negativeNumberHandler.Handle(input);
 
             return sumHandler.Handle(input);
         }
         
-        private static void CheckForNegatives(string input)
-        {
-            var matches = Regex.Matches(input, @"-\d+")
-                .Select(o => o.Value)
-                .ToList();
-            var found = string.Join(", ", matches);
-            if (matches.Count > 0)
-                throw new NegativesNotAllowedException($"Negative numbers not allowed. Found: {found}.");
-        }
-
         private static bool InputIsEmpty(string input)
         {
             return input == "";
---
+++ StringCalcKata/NegativeNumberHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalcKata
+{
+    public class NegativeNumberHandler
+    {
+        public void Handle(string input)
+        {
+            var matches = Regex.Matches(input, @"-\d+")
+                .Select(o => o.Value)
+                .ToList();
+            var found = string.Join(", ", matches);
+            if (matches.Count > 0)
+                throw new NegativesNotAllowedException($"Negative numbers not allowed. Found: {found}.");
+        }
+    }
+}
